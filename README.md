df81589571471cbe63bc0fe55ca85e54f3c1c2fb
DALA746 <darya.lapata@gmail.com>
2023-03-02 10:18:39 +0100

    wrote readme and cleaned comments

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Express API Project
 
-This week project I started my backend journey by creating an API using Express. This API have RESTful endpoints which return either an array of data, or a single item.
+This project developed during my time at technigo boot camp. During development, I gained an insight into how the backend works and created my first API. In this latest update, I added a search function with the possibility to search by title.
 
-## What I learned
+## What did I learn during this backend journey?
 
-- How to install and setup an Express server.
-- How to deploy Node projects.
-- How to build an API in Node using Express
-- How to create routes in Express
-- Practice data manipulation in JavaScript - selecting, filtering, and limiting arrays
+- How to set install och set up an Express server.
+- How to deploy Node projects. I am using Railway free plan.
+- How to build an API in Node using Express.js
+- Data manipulation in JavaScript - selecting, filtering, and limiting arrays.
+
+I also created a frontend project just for the demonstration of this API. You can find it in the link below.
 
 ## View it live
 
-Backend: https://dls-shows-api.herokuapp.com/
-Frontend: https://dl-shows-api.netlify.app/
+Backend: https://project-express-api-production.up.railway.app
+Frontend demo of API: https://movietime-api.netlify.app/
--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-/* eslint-disable implicit-arrow-linebreak */
-/* eslint-disable function-paren-newline */
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
 import netflixData from './data/netflix-titles.json';
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-
+// defining a port where the app will run.
 const port = process.env.PORT || 8080;
 // initialazing new express server
 const app = express();
@@ -20,36 +16,21 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Defining routes here
-// this is an endpoint
 app.get('/', (req, res) => {
   res.send({
-    title: 'Welcome to DL:s shows API!',
+    title: 'Welcome to MovieTime API!',
     description: {
-      endpoint1: {
-        'Returns the entire shows array': '/shows',
-        'Returns shows by a specific title, replace text inside <> with your text':
-          '/shows?title=<title>',
-        'Returns shows by a specific country, replace text inside <> with your text':
-          '/shows?country=<country>',
-        'Returns shows by a specific type (tv shows or movies), replace text inside <> with your text':
-          '/shows?type=<type>',
-        'Returns shows by a specific year, replace text inside <> with your text':
-          '/shows?year=<year here>',
-        'Set limit and page of a response, replace text inside <> with your text':
-          '/shows?page=<page number>&limit=<limit number>'
-      },
-      endpoint2: {
-        'This endpoint returns shows with a specific id. Replace :id with a number of the show.':
-          '/shows/id/:id'
-      }
+      'You can read documentation and view a frontend demo of this API here':
+        'https://movietime-api.netlify.app/documentation'
     }
   });
 });
 
 app.get('/shows/search', (req, res) => {
   const { title } = req.query;
-  const arrayToSendToUser = netflixData.filter((item) =>
-    item.title.toLocaleLowerCase().includes(title.toLocaleLowerCase)
+  const arrayToSendToUser = netflixData.filter(
+    (item) => item.title.toLocaleLowerCase().includes(title.toLocaleLowerCase)
+    /* eslint-disable func-call-spacing */
   );
 
   if (arrayToSendToUser.length === 0) {
@@ -62,51 +43,29 @@ app.get('/shows/search', (req, res) => {
   });
 });
 
-// NOTES ABOUT QUERY & PARAMS
-// different between params and query is that params are mandatory
-// if you create an endpoint that takes params , you have to include that
-// param in your endpoint. Otherwise it's not going to trigger that endpoint.
-// Query params are optional, you can include them, but you dont have to
-// the endpint is going to work with or without query in end point path
-
-// path params uses when you want only send one object from the data
-// query params uses when you about filtering a data and send back multiple amount,
-// just a smaller array of the records
-
-// FIRST ENDPOINT
-// call back function inside of get() communicates with fronted via those two argument(req and res)
-// res = is what we send back to the frontend
-// req = it's handaling send to the backend like a specific query for ex
 app.get('/shows', (req, res) => {
-  // req is an object with properties such as query and params,
-  // both query and params are an empty object from the begging
-  // every query parameter consist of two part, key and value, like an object
   const { title, country, type, year, page, limit } = req.query;
 
-  // creating this variable, because we will always return the same array to the user
-  // note to me: it is my a currentQuestion variable from previous project?
   let arrayToSendToUser = netflixData;
 
-  // can i combine title, country and type into one if?
   if (title) {
-    /* toLowerCase() sets the data in type array data to lowercase, 
-    then includes() returns true if a string contains type from url, 
-    includes() is almost the same as indexOf(), indecOf return number, when includes a boolean, 
-    a part of the string that we send should be in the database */
-    arrayToSendToUser = arrayToSendToUser.filter((item) =>
-      item.title.toLowerCase().includes(title.toLowerCase())
+    arrayToSendToUser = arrayToSendToUser.filter(
+      (item) => item.title.toLowerCase().includes(title.toLowerCase())
+      /* eslint-disable func-call-spacing */
     );
   }
 
   if (country) {
-    arrayToSendToUser = arrayToSendToUser.filter((item) =>
-      item.country.toLowerCase().includes(country.toLowerCase())
+    arrayToSendToUser = arrayToSendToUser.filter(
+      (item) => item.country.toLowerCase().includes(country.toLowerCase())
+      /* eslint-disable func-call-spacing */
     );
   }
 
   if (type) {
-    arrayToSendToUser = arrayToSendToUser.filter((item) =>
-      item.type.toLowerCase().includes(type.toLowerCase())
+    arrayToSendToUser = arrayToSendToUser.filter(
+      (item) => item.type.toLowerCase().includes(type.toLowerCase())
+      /* eslint-disable func-call-spacing */
     );
   }
 
@@ -149,19 +108,17 @@ app.get('/shows', (req, res) => {
 });
 
 // SECOND ENDPOINT
-// get a specific show based on id, using params
-// :id is like a placeholder different id:s that will show up in the url field in the browser
+// get a specific show based on id, using params, :id is like a placeholder
 app.get('/shows/id/:id', (req, res) => {
   // params in this case are :id, params is something that we can get from the request
   // all params comes as strings
   const { id } = req.params;
-  // "+" turns the string in to a number.
+  // "+" turns a string in to a number.
   const showOnlyOneShow = netflixData.find((show) => show.show_id === +id);
 
   if (!showOnlyOneShow) {
     res.status(404).json({
       response: 'No show found with that id',
-      // boolean property,
       success: false
     });
   } else {
@@ -176,6 +133,5 @@ app.get('/shows/id/:id', (req, res) => {
 // Start the server
 // passing port variable that we difined on line 18
 app.listen(port, () => {
-  // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`);
 });
